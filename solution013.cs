9c29ee738f9c069927cf445857898e68cecab065
Jteve-Sobs <46194843+Jteve-Sobs@users.noreply.github.com>
2021-11-18 23:04:47 +0100

    Create solution013.cs

---
+++ solution013.cs
@@ -0,0 +1,35 @@
+// Roman to Integer
+// Runtime: 80 ms, faster than 71.79% of C# online submissions for Roman to Integer.
+// Memory Usage: 39.6 MB, less than 12.75% of C# online submissions for Roman to Integer.
+
+public class Solution {
+    public static readonly Dictionary<char, int> RomanNumberDictionary = new Dictionary<char, int>
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 },
+        };
+    
+    public int RomanToInt(string s) {
+        
+        int total = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (i + 1 < s.Length && RomanNumberDictionary[s[i]] < RomanNumberDictionary[s[i + 1]])
+            {
+                total -= RomanNumberDictionary[s[i]];
+            }
+            else
+            {
+                total += RomanNumberDictionary[s[i]];
+            }
+        }
+        
+        return total;
+    }
+}
