6e7f354c54ebc09ea78976a71ebcd99be1b953af
Jteve-Sobs <46194843+Jteve-Sobs@users.noreply.github.com>
2021-11-18 14:40:49 +0100

    Create solution.cs

---
+++ solution.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        
+        for (int i = 0; i < nums.Count(); i++)
+        {
+            for(int j = i + 1; j < nums.Count(); j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    return new int[2] { i, j };
+                }
+            }
+        }
+            
+        return null;
+    }
+}
